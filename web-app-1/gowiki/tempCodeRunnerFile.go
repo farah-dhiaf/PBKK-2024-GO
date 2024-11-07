82de8c7b7ecf55ff4c0761e07e8aacdbd85ecf9c
Farah Dhia Fadhila <farahdhia52@gmail.com>
2024-11-07 09:23:37 +0700

    add gowiki

---
+++ web-app-1/gowiki/tempCodeRunnerFile.go
@@ -0,0 +1,6 @@
+func main() {
+//     p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+//     p1.save()
+//     p2, _ := loadPage("TestPage")
+//     fmt.Println(string(p2.Body))
+// }
